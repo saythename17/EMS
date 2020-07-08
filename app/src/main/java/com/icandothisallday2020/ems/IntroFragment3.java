d6506e2732e02188d7c012e31520f0fdf3d3188f
FANXY_ON <zion.h719@gmail.com>
2020-07-08 17:07:18 +0900

    commit

--- app/src/main/java/com/icandothisallday2020/ems/IntroFragment3.java
+++ app/src/main/java/com/icandothisallday2020/ems/IntroFragment3.java
@@ -1,9 +1,11 @@
 package com.icandothisallday2020.ems;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,14 @@ public class IntroFragment3 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_intro3,container,false);
+        ImageView introX=view.findViewById(R.id.introX);
+        introX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+            }
+        });
+
         return view;
     }
 }
