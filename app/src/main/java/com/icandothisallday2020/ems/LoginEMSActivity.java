6b3b1ce6b3be1fff07a106c14db5e99f16c98140
FANXY_ON <zion.h719@gmail.com>
2020-09-01 16:51:01 +0900

    design ems login

--- app/src/main/java/com/icandothisallday2020/ems/LoginEMSActivity.java
+++ app/src/main/java/com/icandothisallday2020/ems/LoginEMSActivity.java
@@ -2,7 +2,9 @@ package com.icandothisallday2020.ems;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginEMSActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class LoginEMSActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_e_m_s);
+    }
+
+    public void emsLogin(View view) {
     }
 }
